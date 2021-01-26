b62330aa1ada55e357e53327da1a3a16d76c760e
Joey Hua <joeyhua17@gmail.com>
2021-01-25 23:03:57 -0800

    worked on some styling

--- components/Panel.js
+++ components/Panel.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, Button, Linking } from 'react-native';
 
+import { screenWidth, screenHeight, fontScale, verticalScale } from './Scaler';
+
 export default function Panel({
   imgUri,
   title,
@@ -17,7 +19,7 @@ export default function Panel({
           uri: imgUri
         }}
       />
-      <View style={{display:'flex', paddingLeft:30, paddingRight:30}}>
+      <View style={styles.infoView}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.summary}>{summary}</Text>
         <View style={styles.readButton}>
@@ -39,19 +41,18 @@ export default function Panel({
           uri: imgUri
         }}
       />
-      <View style={{display:'flex', paddingLeft:30, paddingRight:30}}>
+      <View style={styles.infoView}>
         <Text style={styles.title}>{title}</Text>
         <Text style={styles.summary}>{summary}</Text>
-
         <View style={styles.articleList}>
           {articles.map((a,i) => 
             <View style={styles.articleListRow} key={`article-${i}`}>
               <Text 
-                style={{fontWeight:'bold'}}
+                style={styles.articleHeadline}
                 onPress={() => Linking.openURL(a.url)}
               >{a.headline}</Text>
-              <Text>{a.writers}</Text>
-              <Text>{a.bias_score}</Text>
+              <Text style={styles.articleWriters}>{a.writers}</Text>
+              <Text style={styles.articleScore}>{a.bias_score}</Text>
             </View>
           )}
         </View>
@@ -76,18 +77,41 @@ export default function Panel({
 
 const styles = StyleSheet.create({
   containerCollapsed: {
+    marginTop:20,
     display:'flex',
     backgroundColor:'#D0D0D0',
     alignSelf:'stretch',
     alignItems:'center',
-    height:430
+    height:verticalScale(430),
   },
   containerExpanded: {
+    marginTop:20,
     display:'flex',
     backgroundColor:'#D0D0D0',
     alignSelf:'stretch',
     alignItems:'center',
-    paddingBottom:30
+    paddingBottom:30,
+    width:screenWidth(),
+  },
+  image: {
+    height:200,
+    alignSelf:'stretch'
+  },
+  infoView: {
+    display:'flex', 
+    paddingLeft:30,
+    paddingRight:30, 
+    width:screenWidth() // keeps view from widening
+  },
+  title: {
+    fontWeight:'bold',
+    fontSize:fontScale(20),
+    marginTop:10
+  },
+  summary: {
+    fontSize:fontScale(15),
+    marginTop:5,
+    lineHeight:25
   },
   articleList: {
     marginTop:10,
@@ -100,19 +124,15 @@ const styles = StyleSheet.create({
     alignItems:'center',
     height:40,
   },  
-  image: {
-    height:200,
-    alignSelf:'stretch'
-  },
-  title: {
+  articleHeadline: {
     fontWeight:'bold',
-    fontSize:20,
-    marginTop:10
+    fontSize:fontScale(15)
   },
-  summary: {
-    fontSize:15,
-    marginTop:5,
-    lineHeight:25
+  articleWriters: {
+    fontSize:fontScale(15)
+  },
+  articleScore: {
+    fontSize:fontScale(15)
   },
   readButton: {
     width:100,
@@ -121,6 +141,6 @@ const styles = StyleSheet.create({
     display:'flex',
     justifyContent:'center',
     borderRadius:5,
-    marginTop:20
+    marginTop:20,
   }
 })
